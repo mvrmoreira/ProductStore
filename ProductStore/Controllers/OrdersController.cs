bb48cb082f42599f3c0e1582789de197385a669d
Matheus Moreira <mvrmoreira@gmail.com>
2014-06-30 14:14:49 -0300

    Part 6: Creating Product and Order Controllers

---
+++ ProductStore/Controllers/OrdersController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using ProductStore.Models;
+
+namespace ProductStore.Controllers
+{
+    [Authorize]
+    public class OrdersController : ApiController
+    {
+        private OrdersContext db = new OrdersContext();
+
+        // GET api/Orders
+        public IEnumerable<Order> GetOrders()
+        {
+            return db.Orders.Where(o => o.Customer == User.Identity.Name);
+        }
+
+        // GET api/Orders/5
+        public OrderDTO GetOrder(int id)
+        {
+            Order order = db.Orders.Include("OrderDetails.Product").First(o => o.Id == id && o.Customer == User.Identity.Name);
+            if (order == null)
+            {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return new OrderDTO() {
+                Details = from d in order.OrderDetails
+                          select new OrderDTO.Detail() {
+                              ProductID = d.Product.Id,
+                              Product = d.Product.Name,
+                              Price = d.Product.Price,
+                              Quantity = d.Quantity
+                          }
+            };
+        }
+
+        // PUT api/Orders/5
+        //public HttpResponseMessage PutOrder(int id, Order order)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+        //    }
+
+        //    if (id != order.Id)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.BadRequest);
+        //    }
+
+        //    db.Entry(order).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.OK);
+        //}
+
+        // POST api/Orders
+        public HttpResponseMessage PostOrder(OrderDTO dto)
+        {
+            if (ModelState.IsValid)
+            {
+                var order = new Order() {
+                    Customer = User.Identity.Name,
+                    OrderDetails = (from item in dto.Details select new OrderDetail(){
+                                        ProductId = item.ProductID,
+                                        Quantity = item.Quantity
+                                    }).ToList()
+                };
+
+                db.Orders.Add(order);
+                db.SaveChanges();
+
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, order);
+                response.Headers.Location = new Uri(Url.Link("DefaultApi", new { id = order.Id }));
+                return response;
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+            }
+        }
+
+        // DELETE api/Orders/5
+        //public HttpResponseMessage DeleteOrder(int id)
+        //{
+        //    Order order = db.Orders.Find(id);
+        //    if (order == null)
+        //    {
+        //        return Request.CreateResponse(HttpStatusCode.NotFound);
+        //    }
+
+        //    db.Orders.Remove(order);
+
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException ex)
+        //    {
+        //        return Request.CreateErrorResponse(HttpStatusCode.NotFound, ex);
+        //    }
+
+        //    return Request.CreateResponse(HttpStatusCode.OK, order);
+        //}
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProductStore/Controllers/ProductsController.cs
@@ -0,0 +1,43 @@
+﻿using ProductStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductStore.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private OrdersContext db = new OrdersContext();
+
+        private IQueryable<ProductDTO> MapProducts() {
+            return from p in db.Products
+                   select new ProductDTO() {
+                       Id = p.Id,
+                       Name = p.Name,
+                       Price = p.Price
+                   };
+        }
+
+        public IEnumerable<ProductDTO> GetProducts() {
+            return MapProducts().AsEnumerable();
+        }
+
+        public ProductDTO GetProduct(int i) {
+            var product = (from p in MapProducts() where p.Id == 1 select p).FirstOrDefault();
+
+            if (product == null) {
+                throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+            }
+
+            return product;
+        }
+
+        protected override void Dispose(bool disposing) {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
